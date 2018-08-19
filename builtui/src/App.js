6ebeab8446c1c2b5b9526b60e7dc2a5b169ba48f
Khushboopandit <khushboo17@navgurukul.org>
2018-08-19 18:37:57 +0530

    solving tarnery operators problem

--- builtui/src/App.js
+++ builtui/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';     
-import logo from './logo.svg';
 import './App.css';
-import LoginComponent from './LoginComponent.js'
+import Signup from './Signup.js'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginComponent/>
+        <Signup/>
       </div>
     );
   }
