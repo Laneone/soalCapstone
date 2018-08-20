730fa20b5bad98ce46a2f557178b8bb4ffb700db
Khushboopandit <khushboo17@navgurukul.org>
2018-08-20 18:12:03 +0530

    change ternary operator to routes

--- builtui/src/App.js
+++ builtui/src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';     
 import './App.css';
-import Signup from './Signup.js';
 import LoginComponent from './LoginComponent.js';
+import Signup from './Signup.js';
+import { BrowserRouter as Router, Route  } from 'react-router-dom';
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state={
-      isregister:false
-    }
-  }
-
-  onRegisterClick=()=>{
-    this.setState({isregister: true})
-  }
   
+
   render() {
     return (
-      <div className="App">
-        {this.state.isregister===true ? <Signup/>:<LoginComponent onRegisterClick={this.onRegisterClick}/>} 
-      </div>
+      <Router>
+        <div className="App">
+            <Route path  = "/LoginComponent" component={LoginComponent}/>
+            <Route Exact path  = "/Signup" component={Signup}/>        
+        </div>
+      </Router>
     );
   }
 }
--- builtui/src/LoginComponent.js
+++ builtui/src/LoginComponent.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import './LoginComponent.css'
+import {Link  } from 'react-router-dom';
+
+
 const FormItem = Form.Item; 
                                                                                                                                                                     
 class LoginForm extends React.Component {
@@ -17,6 +20,7 @@ class LoginForm extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
+     
       <div className="loginpage">
       <Form onSubmit={this.handleSubmit} className="login-form">
        <p className="loginpapier">Papier</p>
@@ -35,20 +39,15 @@ class LoginForm extends React.Component {
           )}
         </FormItem>
         <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">Forgot password</a><br/>
+         
           <Button type="primary" htmlType="submit" className="login-form-button">
             Log in
-          </Button>
-         <span className="whiteOnOr"> Or</span> <div onClick={this.props.onRegisterClick}>register now!</div>
+          </Button><br/>
+         <span className="whiteOnOr"> New member?</span> <span className="signupBtn" onClick={this.props.onRegisterClick}><Link to="/Signup">Sign up!</Link></span>
         </FormItem>
       </Form>
       </div>
+      
     );
   }
 }
